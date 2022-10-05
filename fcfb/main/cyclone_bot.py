c29af781afa1e2f1d0c9fe99d5ccefa32e53e288
Andrew Kicklighter <apkick@apkicklighter-hp.kicklighter.com>
2022-10-05 03:06:58 -0500

    Fix path issue

--- fcfb/main/cyclone_bot.py
+++ fcfb/main/cyclone_bot.py
@@ -1,16 +1,27 @@
 """
-1313 Bot handles the scoreboard and various other score queries for FCFB
+Cyclone Bot handles the scoreboard and various other score queries for FCFB
 
 @author: apkick
 
 """
 
+import sys
 import os
+ 
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+ 
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
 
-from fcfb.database.retrieve_from_database import *
-from fcfb.reddit.reddit_games import add_games_from_wiki
-from fcfb.reddit.reddit_administration import *
-from fcfb.discord.discord_administration import *
+from reddit.reddit_administration import *
+from discord.discord_administration import *
 
 if __name__ == '__main__':
     r = login_reddit()
